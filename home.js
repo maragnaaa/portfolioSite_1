8e901e9082eafe86362462ae4604daaa53474efe
maragnaaa <gustavomaragna88@gmail.com>
2025-05-10 00:24:41 -0300

    Implementação inicial do Swiper API

--- home.js
+++ home.js
@@ -1,12 +1,32 @@
 var swiper = new Swiper(".swiper", {
-  cssMode: true,
+  spaceBetween: 30,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  keyboard: true,
+  centeredSlides: true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+     breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    },
+    1188: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+  }
 });
+
