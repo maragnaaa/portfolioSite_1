1080f2deeb673b7b6efefa7c9c93c43361a46edd
maragnaaa <gustavomaragna88@gmail.com>
2025-05-12 21:02:18 -0300

    Implementação do Swiper JS e atualização das animações do header

--- home.js
+++ home.js
@@ -1,32 +1,18 @@
-var swiper = new Swiper(".swiper", {
-  spaceBetween: 30,
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-     breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 18
-    },
-    1188: {
-      slidesPerView: 3,
-      spaceBetween: 24
-    }
-  }
-});
-
+    });
